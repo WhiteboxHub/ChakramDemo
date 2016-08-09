100f69fd48c38d2eca40eb46d9730b39c54488de
lenovo <engr1@Innova->
2016-08-09 14:03:12 -0700

    Venkatesh

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+    // Add the grunt-mocha-test tasks.
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    grunt.initConfig({
+        // Configure a mochaTest task
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    captureFile: 'results.txt',
+                    quiet: false,
+                    clearRequireCache: false
+                },
+                src: ['test/*.js']
+            }
+        }
+    });
+
+    grunt.registerTask('default', 'mochaTest');
+
+};
